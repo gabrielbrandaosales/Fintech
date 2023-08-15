e3cdab81b49e0613cb84e73b08d4438b34937975
Gabriel Brandão <gabrielbrandaosales@hotmail.com>
2023-08-15 09:01:40 -0300

    feat: DateRange Input

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import Header from './Components/Header';
 import Sidenav from './Components/Sidenav';
+import { DataContextProvider } from './Context/DataContext';
 import Resumo from './Pages/Resumo';
 import './Style.css';
 
 function App() {
   return (
-    <>
-      <Sidenav />
-      <main>
-        <Header />
-        <Resumo />
-      </main>
-    </>
+    <DataContextProvider>
+      <>
+        <Sidenav />
+        <main>
+          <Header />
+          <Resumo />
+        </main>
+      </>
+    </DataContextProvider>
   );
 }
 
